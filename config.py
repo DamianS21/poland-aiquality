55fad45e8cf6a54daa3acbac6965e585966f151e
Damian Szumski <damian.szumski@yahoo.com>
2021-12-28 22:42:51 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+STATIONS_HTML_ = 'https://api.gios.gov.pl/pjp-api/rest/station/'
+SENSORS_HTML_ = 'https://api.gios.gov.pl/pjp-api/rest/station/sensors/'
+DATA_SENSOR_HTML_ = 'https://api.gios.gov.pl/pjp-api/rest/data/getData/'
